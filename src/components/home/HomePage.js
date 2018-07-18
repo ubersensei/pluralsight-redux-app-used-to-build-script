8cfe9dfc6042687c53ff2f6cd811bbcabce289d9
Kaya <ubersensei@gmail.com>
2018-07-18 18:00:09 +0800

    update with image

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router';
+import thumbnail from '../../images/net.png';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h1>Pluralsight Administration</h1>
-                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            <div>
+                <img id="thumbnail" src={thumbnail}/>
+                <div className="jumbotron">
+                    <h1>Pluralsight Administration</h1>
+                    <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+                    <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+                </div>
             </div>
+
         );
     }
 }
